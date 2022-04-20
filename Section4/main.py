8ced9dfca93e33fc686bbfb71a095cf3ab82d5f1
Aditya <arizka.ashar@gmail.com>
2022-04-20 22:26:04 +0700

    Path parameters

--- Section4/main.py
+++ Section4/main.py
@@ -3,15 +3,35 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 async def index():
     return {"message": "Hello World"}
 
 
 
+# order path is important!!
+@app.get("/countries/indonesia")
+async def indo():
+    return {"message", "This is Indonesia"}
+
+# Path parameter
+@app.get("/countries/{country_name}")
+async def country(country_name: str):  #async def country(country_name: int):
+    return {"country_name": country_name}
+
+
+# @app.get("/countries/indonesia")
+# async def indo():
+#     return {"message", "This is Indonesia"}
+
+
+
+
+
 if __name__ == "__main__":
-    uvicorn.run(app)
+    #uvicorn.run(app, reload = True)
+    uvicorn.run("main:app", reload = True)
 
 
-    # OR
+    # OR from command line
     # uvicorn main:app --reload
