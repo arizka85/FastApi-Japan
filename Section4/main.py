3145a04b15df2fc93822fc928ebd4d9647d7d05c
Aditya <arizka.ashar@gmail.com>
2022-04-20 22:57:34 +0700

    Query parameters

--- Section4/main.py
+++ Section4/main.py
@@ -27,6 +27,53 @@ async def country(country_name: str):  #async def country(country_name: int):
 
 
 
+# Query parameter - required parameter!
+"""
+http://127.0.0.1:8000/negara/?country_name=indonesia&country_no=62
+"""
+# @app.get("/negara/")
+# async def negara(country_name: str, country_no: int):
+#     return {
+#         "country_name": country_name,
+#         "country_no": country_no
+#     }
+
+
+# Query parameter - default parameter!
+"""
+http://127.0.0.1:8000/negara/
+http://127.0.0.1:8000/negara/?country_name=japan&country_no=1
+"""
+# @app.get("/negara/")
+# async def negara(country_name: str = "indonesia", country_no: int = 62):
+#     return {
+#         "country_name": country_name,
+#         "country_no": country_no
+#     }
+
+
+
+# Path parameter & Query parameter
+@app.get("/negara/{country_name}")
+async def negara2(country_name: str, city_name: str):  # async def negara2(country_name: str = "indonesia", city_name: str = "surabaya"):
+    return {
+        "country_name": country_name,
+        "city_name": city_name
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     #uvicorn.run(app, reload = True)
