4b6a6e7692c0f24e5d0efdbcf8d4aee4461423f9
Aditya <arizka.ashar@gmail.com>
2022-04-23 22:58:37 +0700

    Data display elements

--- Section6/app.py
+++ Section6/app.py
@@ -1,24 +1,26 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Demo App")
-"Hello World"
-
-
-st.title("Sample App")
 st.write("Sample App")
 
-st.markdown("# Sample App1")
-st.markdown("## Sample App2")
-st.markdown("### Sample App3")
+
+df = pd.DataFrame({
+    "satu": [1, 2, 3, 4],
+    "dua": [-1, -2, -3, -4]
+})
+
+st.dataframe(df)
 
 
-st.code("a = 123", language="python")
-st.code("""
-import pandas as pd
-import numpy as np
-pd.DataFrame()
+st.json(
+{
+    'data': {
+        'name': 'abc',
+        'age': 123
+    }
+}
 
-""")
+)
 
 
 # streamlit run app.py
