596d6e788339aa3c83f3f4f1b61cea857ffc1936
Aditya <arizka.ashar@gmail.com>
2022-04-23 23:19:31 +0700

    Input widgets

--- Section6/app.py
+++ Section6/app.py
@@ -2,21 +2,31 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Sample App")
+st.button("Click me")
+
+if st.button("Test !"):
+    st.write("Hello Test")
 
 
-st.write(np.random.rand(20, 3))
+
+st.checkbox("Checkbox 1")
 
 
 
-df = pd.DataFrame(
-    np.random.rand(20, 3),
-    columns=['a', 'b', 'c']
 
-)
-# st.line_chart(df)
-# st.area_chart(df)
-st.bar_chart(df)
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+st.write('You selected:', options)
+
+
+
+
+
+
+
+
 
 
 
