d654d722013892938e115efb3055b719e549abaa
Aditya <arizka.ashar@gmail.com>
2022-04-23 22:50:09 +0700

    Text elements

---
+++ Section6/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.write("Demo App")
+"Hello World"
+
+
+st.title("Sample App")
+st.write("Sample App")
+
+st.markdown("# Sample App1")
+st.markdown("## Sample App2")
+st.markdown("### Sample App3")
+
+
+st.code("a = 123", language="python")
+st.code("""
+import pandas as pd
+import numpy as np
+pd.DataFrame()
+
+""")
+
+
+# streamlit run app.py
+
